a8f934ebd5519e452509de649f3b51087b842a28
Bogdan <bogdanguk@gmail.com>
2019-08-26 12:58:59 +0300

    created interface with constrains and added some IBOutlets and IBActions

--- ShakeMeSilly/ViewController.swift
+++ ShakeMeSilly/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    var timer = Timer()
+    
+    var timeInt = 10
+    var scoreInt = 0
+    
+    var imageInt = 1
+    var modeInt = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    
+    
+    @IBAction func startGame(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
 
 }
 
